550b3973209be96a3862743f24e45fb79172acfe
dongsu8142 <dongsu8142@kakao.com>
2022-08-30 04:38:33 +0900

    change keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,7 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
-keymap('n', '<C-t>', '<cmd>NvimTreeToggle<CR>', opts)
+keymap('n', '<C-t>', '<cmd>NvimTreeFocus<CR>', opts)
 keymap('n', '<C-f>', '<cmd>Format<CR>', opts)
 keymap('n', '<C-s>', '<cmd>w<CR>', opts)
 keymap('n', 'ff', '<cmd>Telescope find_files<CR>', opts)
