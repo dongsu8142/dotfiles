6ec0c311a1bebb4be04611f4eb9baa6ed9750e62
dongsu8142 <dongsu8142@kakao.com>
2022-08-30 05:34:58 +0900

    lsp: enable update on insert

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,3 +13,10 @@ for _, server in pairs(servers) do
     capabilities = capabilities
   })
 end
+
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics,
+  {
+    update_in_insert = true
+  }
+)
