de08b5f5446bade6c06fcbb0700d171b9a478bf7
dongsu8142 <dongsu8142@kakao.com>
2022-09-01 19:51:12 +0900

    add jsonls lsp server

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,4 +1,4 @@
-local servers = { 'tsserver', 'html' }
+local servers = { 'tsserver', 'html', 'jsonls' }
 local lspconfig = require("lspconfig")
 local augroup = vim.api.nvim_create_augroup('LspDocumentHighlight', { clear = true })
 
@@ -9,7 +9,7 @@ require('mason-lspconfig').setup({
 })
 
 local on_attach = function(client, bufnr)
-  if client.supports_method('textDocument/documentHighlightProvider') then
+  if client.supports_method('textDocument/documentHighlight') then
     vim.api.nvim_clear_autocmds({ buffer = bufnr, group = augroup })
     vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
       callback = function() vim.lsp.buf.document_highlight() end,
@@ -26,10 +26,16 @@ end
 
 for _, server in pairs(servers) do
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-  lspconfig[server].setup({
-    capabilities = capabilities,
-    on_attach = on_attach
-  })
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+  local opts = {
+    on_attach = on_attach,
+    capabilities = capabilities
+  }
+  local has_custom_opts, server_custom_opts = pcall(require, 'plugins.lsp.settings.' .. server)
+  if has_custom_opts then
+    opts = vim.tbl_deep_extend('force', opts, server_custom_opts)
+  end
+  lspconfig[server].setup(opts)
 end
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
