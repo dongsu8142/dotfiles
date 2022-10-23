4f1673d698f072b6d1e2cfb0d5bf343b74138943
dongsu8142 <dongsu8142@kakao.com>
2022-10-23 12:08:59 +0900

    fix: cmp_nvim_lsp

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -34,7 +34,7 @@ local on_attach = function(client, bufnr)
 end
 
 for _, server in pairs(servers) do
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
   local opts = {
     on_attach = on_attach,
