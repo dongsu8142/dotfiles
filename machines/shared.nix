6c3aec90becb017aa54de4e4245b6bf8335f674a
dongsu8142 <dongsu8142@naver.com>
2023-06-22 14:33:35 +0900

    Add podman

--- machines/shared.nix
+++ machines/shared.nix
@@ -81,6 +81,16 @@
     };
   };
 
+  virtualisation = {
+    podman = {
+      enable = true;
+      dockerCompat = true;
+    };
+    oci-containers = {
+      backend = "podman";
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
