af789de7ac86d107bed8068939a2e92d076b05da
dongsu8142 <dongsu8142@naver.com>
2023-06-27 05:28:14 +0000

    Add hyprland config

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -10,8 +10,17 @@ in
   options.modules.hyprland = { enable = mkEnableOption "hyprland"; };
   imports = [ inputs.hyprland.homeManagerModules.default ];
   config = mkIf cfg.enable {
+    home.file.".config/hypr/config".source = ./config;
     wayland.windowManager.hyprland = {
       enable = true;
+      extraConfig = ''
+        source=~/.config/hypr/config/monitors.conf
+        source=~/.config/hypr/config/settings.conf
+        source=~/.config/hypr/config/rules.conf
+        source=~/.config/hypr/config/binds.conf
+        source=~/.config/hypr/config/theme.conf
+        source=~/.config/hypr/config/startup.conf
+      '';
     };
   };
 }
