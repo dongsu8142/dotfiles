4c368aa508a46f97f45424849c6d314527a784f5
dongsu8142 <dongsu8142@naver.com>
2023-06-28 20:37:26 +0900

    nvidia patch

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -11,8 +11,31 @@ in
   imports = [ inputs.hyprland.homeManagerModules.default ];
   config = mkIf cfg.enable {
     home.file.".config/hypr/config".source = ./config;
+    home.file. ".local/bin/hypr" = {
+      executable = true;
+      text = ''
+      #!/bin/sh
+        export WLR_NO_HARDWARE_CURSORS=1
+        export _JAVA_AWT_WM_NONREPARENTING=1
+        . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+
+        if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
+          PATH="$HOME/.local/bin:$HOME/bin:$PATH"; fi
+
+        if ! [[ "$PATH" =~ "$HOME/.nix-profile/bin:" ]]; then
+          PATH="$HOME/.nix-profile/bin:$PATH"; fi
+
+        export PATH
+        exec nixGL ${pkgs.hyprland}/bin/Hyprland
+      '';
+    };
     wayland.windowManager.hyprland = {
       enable = true;
+      xwayland = {
+        enable = true;
+        hidpi = true;
+      };
+      nvidiaPatches = true;
       extraConfig = ''
         source=~/.config/hypr/config/monitors.conf
         source=~/.config/hypr/config/settings.conf
