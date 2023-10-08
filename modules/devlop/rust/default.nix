4f5061063304527a4d31144b30030c86c499da6c
dongsu8142 <dongsu8142@naver.com>
2023-10-08 20:32:07 +0900

    feat: added rustfmt, clippy

--- modules/devlop/rust/default.nix
+++ modules/devlop/rust/default.nix
@@ -4,7 +4,7 @@
   home = {
     packages = with pkgs; [
       (rust-bin.selectLatestNightlyWith( toolchain: toolchain.default.override {
-        extensions= [ "rust-src" "rust-analyzer" ];
+        extensions= [ "rust-src" "rust-analyzer" "rustfmt" "clippy"  ];
         targets = [ "wasm32-unknown-unknown" "x86_64-unknown-linux-gnu" ];
       }))
       cargo-watch
