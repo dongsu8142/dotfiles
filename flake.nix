5b382e953c4500938959d1e237bfe05a858ec603
dongsu8142 <dongsu8142@naver.com>
2023-11-22 18:35:23 +0900

    chore(update)

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland = {
-      url = "github:hyprwm/Hyprland/v0.31.0";
+      url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     gBar = {
