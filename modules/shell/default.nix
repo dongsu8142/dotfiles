f5c59d465a54d441d6ddb68fd6ff8baef0d87bf4
dongsu8142 <dongsu8142@naver.com>
2024-01-24 17:50:40 +0900

    feat: add starship

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -1,4 +1,5 @@
 [
   ./fish
   ./git
+  ./starship
 ]
---
+++ modules/theme/catppuccin/common/starship/default.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+
+let
+  theme = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/starship/main/palettes/mocha.toml";
+    hash = "sha256-cSaZrSfbk97d2kV3q5dT924MgmUuY8eYIIU0PIygH5w=";
+  };
+in 
+
+{
+  programs.starship = {
+    settings = {
+      character = {
+        success_symbol = "[›](bold green)";
+        error_symbol = "[›](bold red)";
+      };
+      aws = {
+        symbol = "🅰 ";
+      };
+      gcloud = {
+        format = "on [$symbol$active(\($region\))]($style) ";
+        symbol = "🅶 ️";
+      };
+      palette = "catppuccin_mocha";
+    }
+    // builtins.fromTOML (builtins.readFile "${theme}");
+  };
+}
