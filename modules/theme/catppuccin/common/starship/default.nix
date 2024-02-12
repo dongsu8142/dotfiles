21e0c8f3cf35165a8efbab3199249a6b98edf1e2
dongsu8142 <dongsu8142@naver.com>
2024-02-12 15:40:16 +0900

    feat(starship): add_newline false

--- modules/theme/catppuccin/common/starship/default.nix
+++ modules/theme/catppuccin/common/starship/default.nix
@@ -5,11 +5,12 @@ let
     url = "https://raw.githubusercontent.com/catppuccin/starship/main/palettes/mocha.toml";
     hash = "sha256-cSaZrSfbk97d2kV3q5dT924MgmUuY8eYIIU0PIygH5w=";
   };
-in 
+in
 
 {
   programs.starship = {
     settings = {
+      add_newline = false;
       character = {
         success_symbol = "[›](bold green)";
         error_symbol = "[›](bold red)";
