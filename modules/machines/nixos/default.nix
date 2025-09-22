8c239f7819cd91500e9f4170a0e56675070482d0
dongsu8142 <dongsu8142@naver.com>
2025-09-22 11:42:22 +0900

    clean

--- modules/machines/nixos/default.nix
+++ modules/machines/nixos/default.nix
@@ -27,23 +27,4 @@ in {
         (homeManagerCfg true [ ])
       ];
     })) configs);
-  # server = self.inputs.nixpkgs.lib.nixosSystem {
-  #   system = "x86_64-linux";
-  #   specialArgs = { inherit (self) inputs; };
-  #   modules = [
-  #     ../../../configuration.nix
-  #     self.inputs.sops-nix.nixosModules.sops
-  #     self.inputs.home-manager.nixosModules.home-manager
-  #     { nixpkgs.overlays = [ self.inputs.rust-overlay.overlays.default ]; }
-  #     {
-  #       home-manager = {
-  #         useGlobalPkgs = true;
-  #         useUserPackages = true;
-  #         users.hands8142.imports = [ ../../../home.nix ];
-  #         extraSpecialArgs = { inherit (self) inputs; };
-  #         backupFileExtension = "backup";
-  #       };
-  #     }
-  #   ];
-  # };
 }
