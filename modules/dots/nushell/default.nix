e84ee12e166b1c49c645e9ab5b43d0acb85b281b
dongsu8142 <dongsu8142@naver.com>
2025-09-30 02:56:13 +0900

    add nushell config

--- modules/dots/nushell/default.nix
+++ modules/dots/nushell/default.nix
@@ -5,7 +5,17 @@
       enable = true;
       extraConfig = ''
         $env.config = {
-          show_banner: false,
+          show_banner: false
+          buffer_editor: "nvim"
+          history: {
+            file_format: sqlite
+            max_size: 1_000_000
+            sync_on_enter: true
+            isolation: true
+          }
+          completions: {
+            algorithm: "fuzzy"
+          }
         }
       '';
     };
